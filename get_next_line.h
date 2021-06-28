6249285d91c2e7f2e275a741a833163fbef53bf1
Josu Cid gonzalez <jcid-gon@c3r2s6.42urduliz.com>
2021-06-28 12:04:59 +0200

    1st

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin (char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup (const char *src);
+
+#endif
